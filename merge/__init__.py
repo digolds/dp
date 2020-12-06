b51feae326ea47f0cb848734e323ca2b43bc9e30
caroline <ynxuan1990love@sina.cn>
2020-12-06 19:50:31 +0800

    multi csv to xlsx

--- merge/__init__.py
+++ merge/__init__.py
@@ -1,7 +1,9 @@
 from merge import csv_to_xlsx
 from merge import multi_csv_to_single_csv
+from merge import multi_csv_to_single_xlsx
 
 sub_command_map = {
+    'multi-csv2xlsx' : [multi_csv_to_single_xlsx.multiple_csv_to_single_xlsx, multi_csv_to_single_xlsx.parse],
     'csv2csv' : [multi_csv_to_single_csv.multiple_csv_to_single_csv, multi_csv_to_single_csv.parse],
     'csv2xlsx' : [csv_to_xlsx.csv_to_xlsx, csv_to_xlsx.parse]
 }
