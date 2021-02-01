8be84e0ca110ea3eb4ff77fc0a5604f9a2e1c436
slz <zhengsl@cadg.cn>
2021-02-01 18:08:08 +0800

    add operator definetion

--- common.py
+++ common.py
@@ -7,6 +7,17 @@ class MiddleData:
         if arg_count > 1:
             self.file_name = args[1]
 
+class Operator:
+    def __init__(self, in_df, in_args, callable_core):
+        self.in_df = in_df
+        self.in_args = in_args
+        self.callable_core = callable_core
+        self.out_df = None
+
+    def __call__(self):
+        self.out_df = self.callable_core(self.in_df, self.in_args)
+        return self.out_df
+
 if __name__ == '__main__':
     md = MiddleData('abc','llllll')
     print(md.df)
