5bad03685ee1b67ddc91185cc4e340cd88865ad9
slz <zhengsl@cadg.cn>
2021-02-01 17:54:43 +0800

    refactor the code

--- rw/read_write.py
+++ rw/read_write.py
@@ -8,7 +8,10 @@ def create_data_frame(file_name):
         return pd.read_csv(file_name)
 
 def dataframe_to_file(df, args):
-    if args.get('file_name').endswith('.xlsx'):
-        with ExcelWriter(args.get('file_name')) as ew:
+    file_name = args.get('file_name', '')
+    if file_name.endswith('.xlsx'):
+        with ExcelWriter(file_name) as ew:
             df.to_excel(ew, sheet_name=args.get('sheet_name'), index=False)
             return df
+    else:
+        print(f'invalid file: {file_name}')
