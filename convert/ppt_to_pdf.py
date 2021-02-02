55805cf002aa6ea789839bf20b03e2f0386b84f0
slz <zhengsl@cadg.cn>
2021-02-02 15:42:20 +0800

    add ppt to pdf converter

---
+++ convert/ppt_to_pdf.py
@@ -0,0 +1,12 @@
+import win32com.client
+
+def PPTtoPDF(inputFileName, outputFileName, formatType = 32):
+    powerpoint = win32com.client.DispatchEx("Powerpoint.Application")
+    powerpoint.Visible = 1
+
+    if outputFileName[-3:] != 'pdf':
+        outputFileName = outputFileName + ".pdf"
+    deck = powerpoint.Presentations.Open(inputFileName)
+    deck.SaveAs(outputFileName, formatType) # formatType = 32 for ppt to pdf
+    deck.Close()
+    powerpoint.Quit()
